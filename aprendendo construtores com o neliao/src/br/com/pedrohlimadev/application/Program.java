422fbc6d9822aa69c4f78316ba8d3ed33722ec44
pedromm65 <pedromm65@hotmail.com.br>
2022-08-31 23:56:14 -0300

    Sobrecarga

--- aprendendo construtores com o neliao/src/br/com/pedrohlimadev/application/Program.java
+++ aprendendo construtores com o neliao/src/br/com/pedrohlimadev/application/Program.java
@@ -18,10 +18,9 @@ public class Program {
         System.out.print("Price: ");
         double price = sc.nextDouble();
 
-        System.out.print("Quantity in stock: ");
-        int quantity = sc.nextInt();
 
-        Product product = new Product(name, price, quantity);
+
+        Product product = new Product(name, price);
         
         
         System.out.println();
@@ -29,7 +28,7 @@ public class Program {
         System.out.println();
 
         System.out.print("Enter the number of products to be added in stock: ");
-        quantity = sc.nextInt();
+        int quantity = sc.nextInt();
 
         product.addProducts(quantity);
         System.out.println();
