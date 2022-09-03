e44eed5f2bbb0b390c5c16800abb046ea5d72379
pedromm65 <pedromm65@hotmail.com.br>
2022-09-02 21:15:48 -0300

    Finalizando exercicio 1

---
+++ Estudando array com o neliao/src/br/com/pedrohlimadev/application/Program.java
@@ -0,0 +1,36 @@
+package br.com.pedrohlimadev.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entra ai com quantas alturas: ");
+
+        int n = sc.nextInt();
+        
+        double[] vect = new double[n];
+
+        for(int i = 0; i < n; i++) {
+            vect[i] = sc.nextDouble();
+        }
+
+        double sum = 0.0;
+
+        for(int i = 0; i < n; i++) {
+            sum += vect[i];
+            System.out.println();
+        }
+
+        int len = vect.length;
+
+        double averageHeight = sum / len;
+  
+        System.out.printf("AVERAGE HEIGHT: %.2f%n ", averageHeight);
+
+        sc.close();
+    }
+}
