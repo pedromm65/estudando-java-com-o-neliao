d3762489a43e4d405366deb8961880bc3aea9a51
pedromm65 <pedromm65@hotmail.com.br>
2022-09-04 17:51:52 -0300

    Finalizando a aula

---
+++ aprendendo unboxing boxing e wrapper class com o neliao/src/br/com/pedrohlimadev/application/Program.java
@@ -0,0 +1,14 @@
+package br.com.pedrohlimadev.application;
+
+public class Program {
+    public static void main(String[] args) {
+        int x = 20;
+
+        Integer obj = x;
+        System.out.println(obj);
+
+        int y = obj * 2;
+
+        System.out.println(y);
+    }
+}
