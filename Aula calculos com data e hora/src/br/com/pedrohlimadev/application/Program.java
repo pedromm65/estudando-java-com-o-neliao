e5e5fd50141bb31a7904aee9897353d23862e041
pedromm65 <pedromm65@hotmail.com.br>
2022-09-06 20:08:59 -0300

    a

---
+++ Aula calculos com data e hora/src/br/com/pedrohlimadev/application/Program.java
@@ -0,0 +1,46 @@
+package br.com.pedrohlimadev.application;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Program {
+    public static void main(String[] args) {
+        LocalDate d04 = LocalDate.parse("2022-09-05");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+
+        LocalDate pastWeekLocalDate = d04.minusDays(7);
+        LocalDate nextWeekLocalDate = d04.plusDays(7);
+
+
+
+        System.out.println("Past week localDate = " + pastWeekLocalDate);
+        System.out.println("Next week localDate = " + nextWeekLocalDate);
+
+        LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+        LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
+
+        System.out.println("Past week localDateTime = " + pastWeekLocalDateTime);
+        System.out.println("Next week localDateTime = " + nextWeekLocalDateTime);
+
+
+        Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+        Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
+
+        System.out.println("Past week Instance = " + pastWeekInstant);
+        System.out.println("Next week Instance = " + nextWeekInstant);
+
+
+
+        Duration t1 = Duration.between(pastWeekLocalDate.atStartOfDay(), d04.atStartOfDay());
+        Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
+        Duration t3 = Duration.between(pastWeekInstant, d06);
+
+        System.out.println("t1 dias = " + t1.toDays());
+        System.out.println("t2 dias = " + t2.toDays());
+        System.out.println("t3 dias = " + t3.toDays());
+    }
+}
