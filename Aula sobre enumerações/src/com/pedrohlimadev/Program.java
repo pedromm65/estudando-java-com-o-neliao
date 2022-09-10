31cd9ae56707d2da93eb49f336330727f1cf53d2
pedromm65 <pedromm65@hotmail.com.br>
2022-09-10 02:31:02 -0300

    Finalizando aula

---
+++ Aula sobre enumerações/src/com/pedrohlimadev/Program.java
@@ -0,0 +1,21 @@
+package com.pedrohlimadev;
+
+import java.util.Date;
+
+import com.pedrohlimadev.entities.Order;
+import com.pedrohlimadev.entities.enums.OrderStatus;
+
+public class Program {
+    public static void main(String[] args) {
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.DELIVERED;
+
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+
+        System.out.println(os1);
+        System.out.println(os2);
+    }
+}
