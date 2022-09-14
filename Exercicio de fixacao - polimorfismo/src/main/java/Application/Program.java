67e90510844f6b72466fe25bbaae183b9fc90e0c
pedromm65 <pedromm65@hotmail.com.br>
2022-09-14 18:35:22 -0300

    Faznedo

---
+++ Exercicio de fixacao - polimorfismo/src/main/java/Application/Program.java
@@ -0,0 +1,67 @@
+package Application;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        List<Product> list = new ArrayList<>();
+
+        System.out.print("Enter the number of products: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Product #" + i + " data: ");
+            System.out.print("Common, used or imported (c/u/i)? ");
+            char type = sc.next().charAt(0);
+
+            if(type == 'c') {
+                System.out.print("Name: ");
+                String name = sc.next();
+                System.out.print("Price: ");
+                Double price = sc.nextDouble();
+                Product product = new Product(name, price);
+                list.add(product);
+            } else if (type == 'u') {
+                System.out.print("Name: ");
+                String name = sc.next();
+                System.out.print("Price: ");
+                Double price = sc.nextDouble();
+                System.out.print("Manufacture date (DD/MM/YYYY): ");
+                String manuDate = sc.next();
+
+                Product product = new UsedProduct(name, price, sdf.parse(manuDate));
+                list.add(product);
+            } else if (type == 'i') {
+                System.out.print("Name: ");
+                String name = sc.next();
+                System.out.print("Price: ");
+                Double price = sc.nextDouble();
+                System.out.print("Custom fee: ");
+                Double customFee = sc.nextDouble();
+
+                Product product = new ImportedProduct(name, price, customFee);
+                list.add(product);
+            }
+        }
+
+        System.out.println("PRICE TAGS: ");
+        for (Product prod : list) {
+            prod.priceTag();
+        }
+        sc.close();
+    }
+}
---
+++ Exercicio de fixacao - polimorfismo/src/main/java/entities/UsedProduct.java
@@ -0,0 +1,25 @@
+package entities;
+
+import java.util.Date;
+
+public class UsedProduct extends Product {
+    private Date manufactureDate;
+
+    public UsedProduct(String name, Double price, Date manufactureDate) {
+        super(name, price);
+        this.manufactureDate = manufactureDate;
+    }
+
+    public Date getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(Date manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+    @Override
+    public String priceTag() {
+        return getName() + " $ " + String.format("%.2f", getPrice()) + "(Manufacture date:  " + getManufactureDate();
+    }
+}
