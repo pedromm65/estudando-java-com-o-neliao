afe94bcc983563e5c13088b16077884190cd3993
pedromm65 <pedromm65@hotmail.com.br>
2022-09-16 14:28:35 -0300

    Finalizando aula

---
+++ Aula try catch/src/main/java/Application/Program.java
@@ -0,0 +1,37 @@
+package Application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        method1();
+        System.out.println("Fim do progrma");
+    }
+
+
+    public static void method1(){
+        System.out.println("****METHOD1 START*****");
+        method2();
+        System.out.println("****METHOD1 START*****");
+    }
+    public static void method2() {
+        System.out.println("****METHOD2 START*****");
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position");
+            e.printStackTrace();
+            sc.next();
+        } catch (InputMismatchException e){
+            System.out.println("Input error");
+        }
+
+        sc.close();
+        System.out.println("****METHOD2 START*****");
+    }
+}
